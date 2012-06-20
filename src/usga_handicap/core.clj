8813e4a7308e6f9e9e22d1fda4f1f15ec78507a5
Eric Christensen <eric@ecounysis.com>
2012-06-19 22:49:53 -0600

    I can't remember what I changed

--- src/usga_handicap/core.clj
+++ src/usga_handicap/core.clj
@@ -1,17 +1,18 @@
 (ns usga-handicap.core)
 
-(def out-yards 2200)
-(def in-yards 1016)
-(def epy-adj-in 0)
-(def epy-adj-out 0)
-(def obstacle-stroke-value-scratch-in 0)
-(def obstacle-stroke-value-scratch-out 0)
-(def obstacle-stroke-value-bogey-in 0)
-(def obstacle-stroke-value-bogey-out 0)
+;(def out-yards 2200)
+;(def in-yards 1016)
+;(def epy-adj-in 0)
+;(def epy-adj-out 0)
+;(def obstacle-stroke-value-scratch-in 0)
+;(def obstacle-stroke-value-scratch-out 0)
+;(def obstacle-stroke-value-bogey-in 0)
+;(def obstacle-stroke-value-bogey-out 0)
 
-(def obstacle-stroke-value-scratch (+ obstacle-stroke-value-scratch-in obstacle-stroke-value-scratch-out))
-(def obstacle-stroke-value-bogey (+ obstacle-stroke-value-bogey-in obstacle-stroke-value-bogey-out))
-
+;(def obstacle-stroke-value-scratch 
+;     (+ obstacle-stroke-value-scratch-in obstacle-stroke-value-scratch-out))
+;(def obstacle-stroke-value-bogey 
+;     (+ obstacle-stroke-value-bogey-in obstacle-stroke-value-bogey-out))
 
 (defn yardage-rating-function [divisor addend] 
      (fn [eff-playing-yardage]  (+ (/ eff-playing-yardage divisor) addend)))
@@ -22,8 +23,6 @@
 (defn scratch-yardage-rating-9 [eff-playing-yardage] 
       ((yardage-rating-function 220.0 (/ 40.9 2)) eff-playing-yardage))
 
-(scratch-yardage-rating-9 2200)
-
 (defn bogey-yardage-rating [eff-playing-yardage] 
       ((yardage-rating-function 160.0 50.7) eff-playing-yardage))
 
@@ -33,20 +32,22 @@
 (defn rating [yardage-rating obstacle-stroke-value] 
       (+ yardage-rating obstacle-stroke-value))
 
-(def effective-playing-yardage 
-     (+ out-yards in-yards epy-adj-out epy-adj-in))
+(defn seven [] 'seven)
 
-(def yardage-rating-scratch 
-     (scratch-yardage-rating effective-playing-yardage))
+;(def effective-playing-yardage 
+;     (+ out-yards in-yards epy-adj-out epy-adj-in))
 
-(def yardage-rating-bogey 
-     (bogey-yardage-rating effective-playing-yardage))
+;(def yardage-rating-scratch 
+;     (scratch-yardage-rating effective-playing-yardage))
 
-(def usga-course-rating 
-     (rating yardage-rating-scratch obstacle-stroke-value-scratch))
+;(def yardage-rating-bogey 
+;     (bogey-yardage-rating effective-playing-yardage))
 
-(def bogey-course-rating 
-     (rating yardage-rating-bogey obstacle-stroke-value-bogey))
+;(def usga-course-rating 
+;     (rating yardage-rating-scratch obstacle-stroke-value-scratch))
+
+;(def bogey-course-rating 
+;     (rating yardage-rating-bogey obstacle-stroke-value-bogey))
 
 (defn slope-rating [bogey-rating usga-course-rating] 
       (* 5.381 (- bogey-rating usga-course-rating)))
@@ -54,4 +55,7 @@
 (defn slope-rating-9 [bogey-rating-9 usga-course-rating-9] 
       (* (* 5.381 2) (- bogey-rating-9 usga-course-rating-9)))
 
-(def the-slope (slope-rating bogey-course-rating usga-course-rating))
+(defn one-score-handicap-differential [score rating slope]
+      (/ (* (- score rating) 113.0) slope))
+
+;(def the-slope (slope-rating bogey-course-rating usga-course-rating))
